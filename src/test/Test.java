665111dd1fddda5684bf274a330b8f17e482c98a
ccjianci <42882204+ccjianci@users.noreply.github.com>
2018-09-01 17:51:36 +0800

    test

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,5 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
 public class Test{
 	public static void main(String[] args){
-		System.console().writer().println("hello");
+		//System.console().writer().println("hello");
+			Map<String,String> map1=new HashMap<>();
+		for(int i=1;i<14;i++){
+			map1.put("·½¿é"+i," ");
+		}
+		Map<String,String> map2=new HashMap<>();
+		for(int i=1;i<14;i++){
+			map2.put("ºìÌÒ"+i, " ");
+		}
+		Map<String,String> map3=new HashMap<>();
+		for(int i=1;i<14;i++){
+			map3.put("ºÚÌÒ"+i, " ");
+		}
+		Map<String,String> map4=new HashMap<>();
+		for(int i=1;i<14;i++){
+			map4.put("Ã·»¨"+i, " ");
+		}
+		map1.putAll(map2);
+		map1.putAll(map3);
+		map1.putAll(map4);
+		for(Map.Entry<String,String> entry:map1.entrySet()){
+			System.out.println(entry.getKey()+entry.getValue());
+		}
+
+
+
+
+
 	}	
 }
